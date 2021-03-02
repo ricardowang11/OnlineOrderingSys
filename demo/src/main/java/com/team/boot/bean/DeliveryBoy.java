2bacd3dbce492373e262cd5d99f20bd53e1249e5
ricardowang11 <59784603+ricardowang11@users.noreply.github.com>
2021-03-02 10:32:59 +0800

    v1.0.1 小更新1

--- demo/src/main/java/com/team/boot/bean/DeliveryBoy.java
+++ demo/src/main/java/com/team/boot/bean/DeliveryBoy.java
@@ -1,5 +1,6 @@
 package com.team.boot.bean;
 
+import com.team.boot.utils.Address;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,7 +24,7 @@ public class DeliveryBoy {
     private String password;
     private String phone;
 //    骑手负责区域
-    private String address;
+    private Address address;
     //商家和骑手必须实名认证——————实名认证后添加realName属性，否则不允许申请开店（派送外卖）
     private String realName;
 //    骑手当前接到的订单————————利用redis添加持久化手段
